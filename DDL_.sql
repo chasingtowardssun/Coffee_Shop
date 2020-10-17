99d7a75959cae2ea7b620019352e2df9fc6bb7a5
Weilian Shan <wl.shan1990@gmail.com>
2020-10-16 20:47:37 -0700

    modify tables and add example data

--- DDL_.sql
+++ DDL_.sql
@@ -20,15 +20,18 @@ CREATE TABLE Users(
 
 -- Table structure for table `Orders`
 CREATE TABLE Orders(
-    orderID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    orderTime time NOT NULL,
-    orderStatus bool NOT NULL,
+    orderID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userID int(11),
+    CONSTRAINT FK_User FOREIGN KEY (userID)
+    REFERENCES Users(userID),
+    orderTime datetime NOT NULL,
+    orderStatus varchar(255) NOT NULL,
     totalPrice float(8) NOT NULL
 );
 
 -- Table structure for table `Items`
 CREATE TABLE Items(
-    itemID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    itemID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL,
     unitPrice float(8) NOT NULL
 );
@@ -53,7 +56,16 @@ LOCK TABLES `Users` WRITE;
 INSERT INTO Users (name, email, password, phoneNumber)
     VALUES
         ("customer1", "customer1@gmail.com", "$2a$10$I523Fla7Iz.z5KLQqSqdie6Zf/E/wQTMHAGQTHnij5XZ56haTGO.S", "0123456789"),
-        ("customer2", "customer2@gmail.com", "$2a$10$I523Fla7Iz.z5KLQqSqdie6Zf/E/wQTMHAGQTHnij5XZ56haTGO.S", "1234567890");
+        ("customer2", "customer2@gmail.com", "$2a$10$I523Fla7Iz.z5KLQqSqdie6Zf/E/wQTMHAGQTHnij5XZ56haTGO.S", "1234567890"),
+        ("customer3", "customer3@gmail.com", "$2a$10$I523Fla7Iz.z5KLQqSqdie6Zf/E/wQTMHAGQTHnij5XZ56haTGO.S", "1234567890"),
+        ("customer4", "customer4@gmail.com", "$2a$10$I523Fla7Iz.z5KLQqSqdie6Zf/E/wQTMHAGQTHnij5XZ56haTGO.S", "1234567890");
 UNLOCK TABLES;
 
+INSERT INTO coffee_local.Items (itemID, name, unitPrice) VALUES (1, 'apple', 1.2);
+INSERT INTO coffee_local.Items (itemID, name, unitPrice) VALUES (2, 'banana', 1.5);
+
+INSERT INTO coffee_local.Orders (orderID, userID, orderTime, orderStatus, totalPrice) VALUES (1, 1, '2020-10-16 23:40:22', 'completed', 1.5);
+INSERT INTO coffee_local.Orders (orderID, userID, orderTime, orderStatus, totalPrice) VALUES (2, 2, '2020-10-16 22:40:56', 'ready for pick up', 1.2);
+INSERT INTO coffee_local.Orders (orderID, userID, orderTime, orderStatus, totalPrice) VALUES (3, 4, '2020-10-16 21:31:20', 'processing', 2.7);
+INSERT INTO coffee_local.Orders (orderID, userID, orderTime, orderStatus, totalPrice) VALUES (4, 3, '2020-10-16 21:41:42', 'placed', 3);
 
