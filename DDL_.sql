eacf7671edd25ddcbbd5b2a69dda8a401308d820
Weilian Shan <wl.shan1990@gmail.com>
2020-10-16 20:25:21 -0700

    add tables: Items, itemOrder

--- DDL_.sql
+++ DDL_.sql
@@ -3,6 +3,9 @@
 --
 SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `Orders`;
+DROP TABLE IF EXISTS `Items`;
+DROP TABLE IF EXISTS `Item_order`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 
@@ -23,7 +26,23 @@ CREATE TABLE Orders(
     totalPrice float(8) NOT NULL
 );
 
+-- Table structure for table `Items`
+CREATE TABLE Items(
+    itemID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    name varchar(255) NOT NULL,
+    unitPrice float(8) NOT NULL
+);
 
+-- Table structure for table `Item_order`
+CREATE TABLE Item_order(
+    itemOrderID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    orderID int(11),
+    CONSTRAINT FK_ItemOrder FOREIGN KEY (orderID)
+    REFERENCES Orders(orderID),
+    itemID int(11),
+    CONSTRAINT FK_Item FOREIGN KEY (itemID)
+    REFERENCES  Items(itemID)
+);
 
 
 --
