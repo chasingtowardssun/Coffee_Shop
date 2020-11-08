d520bb35da3d8a82bd547820e2e39a233f4aa3f3
Weilian Shan <wl.shan1990@gmail.com>
2020-11-07 23:58:06 -0800

    implement delete item function and username+password automatically shown for log in

--- admin/items.js
+++ admin/items.js
@@ -95,5 +95,22 @@ module.exports = function(){
         });
     });
 
+    // delete an item on menu
+    router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM items WHERE itemID = ?";
+        var inserts = [req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log(error);
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    });
+
     return router;
 }();
