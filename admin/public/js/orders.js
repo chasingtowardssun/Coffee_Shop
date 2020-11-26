bbfa6f515d63f39f19377e792c568d87bdf1edb7
Weilian Shan <wl.shan1990@gmail.com>
2020-11-25 23:00:45 -0800

    fix bug for total price in database

--- admin/public/js/orders.js
+++ admin/public/js/orders.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
         let order_object_name = event.target.id;
         let order_id = order_object_name.split('-')[1];
         let order_status = $('#' + order_object_name).val();
-        let order_price = $('#order_price-' +order_id).val();
+        // let order_price = $('#order_price-' +order_id).val();
 
         console.log(`order ${order_id} changed to ${order_status}`);
-        console.log(order_price);
+        // console.log(order_price);
         // send ajax request to update DB
 
-        updateOrder(order_id, {'orderStatus': order_status, 'totalPrice': order_price});
+        updateOrder(order_id, {'orderStatus': order_status});
     });
 
     $('.price_text').change(function(event) {
@@ -19,10 +19,10 @@ $(document).ready(function(){
         let order_price = $('#' + order_object_name).val();
 
         console.log(`order ${order_id} changed to ${order_status}`);
-        console.log(order_price);
+        // console.log(order_price);
         // send ajax request to update DB
 
-        updateOrder(order_id, {'orderStatus': order_status, 'totalPrice': order_price});
+        updateOrder(order_id, {'orderStatus': order_status});
     });
 
     $('.detail_btn').click(function(event){
