6440754e4f4ff83185f5adf099e6ed9f4476de75
sljtropicfish <59583855+sljtropicfish@users.noreply.github.com>
2020-11-28 20:25:43 -0800

    Fixed item-order foreign key constraint failure problem

--- customer/order.js
+++ customer/order.js
@@ -53,71 +53,72 @@ module.exports = function () {
                 res.end();
             } else {
                 console.log('OK');
+                var priceToPay = 0.0;
+                var orderMark = 0; //if number is 0 for all item, orderMark is 0, meaning user didn't order anything
+                for (var i = 0; i < req.body.itemID.length; i++) {
+                    var sql = "INSERT INTO Item_order (orderID, itemID, itemQuanity) VALUES (?, ?,?)";
+                    var itemID = parseInt(req.body.itemID[i]);
+                    var number = parseInt(req.body.number[i]);
+                    //console.log('itemID: ' + itemID);
+                    //console.log('number: ' + number);
+                    if (number > 0) {
+                        orderMark = 1;
+                        priceToPay = priceToPay + number * parseFloat(req.body.unitPrice[i]);
+                        console.log('price: ' + parseFloat(req.body.unitPrice[i]));
+                        var inserts = [orderID, itemID, number];
+                        console.log("inserts: " + inserts);
+                        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+                            if (error) {
+                                console.log('Not OK');
+                                console.log(JSON.stringify(error));
+                                res.write(JSON.stringify(error));
+                                res.end();
+                            } else {
+                                console.log('OK');
+                            }
+                        });
+                    }
+                }
+                if(orderMark >0){
+                console.log('priceToPay: ' + priceToPay);
+                //update the totalPrice for the order
+                var sql3 = "UPDATE Orders SET totalPrice=? WHERE orderID =?";
+                var inserts3 = [priceToPay, orderID];
+                        sql3 = mysql.pool.query(sql3, inserts3, function (error, results, fields) {
+                            console.log(sql3);
+                            if (error) {
+                                console.log('Not OK');
+                                console.log(JSON.stringify(error));
+                                res.write(JSON.stringify(error));
+                                res.end();
+                            } else {
+                                console.log('OK');
+                                res.redirect('/order/Confirmation');
+                            }
+                        });
+
+                        }
+                //user didn't order, delete the preSet for order
+                if(orderMark == 0){
+                    var sql4 = "DELETE FROM Orders WHERE orderID =?";
+                    var inserts4 = [orderID];
+                    sql4 = mysql.pool.query(sql4, inserts4, function (error, results, fields) {
+                                        console.log(sql4);
+                                        if (error) {
+                                            console.log('Not OK');
+                                            console.log(JSON.stringify(error));
+                                            res.write(JSON.stringify(error));
+                                            res.end();
+                                        } else {
+                                            console.log('OK');
+                                        }
+                                    });
+                                    res.redirect('/order/Order');
+                }
+
             }
         });
 
-        var priceToPay = 0.0;
-        var orderMark = 0; //if number is 0 for all item, orderMark is 0, meaning user didn't order anything
-        for (var i = 0; i < req.body.itemID.length; i++) {
-            var sql = "INSERT INTO Item_order (orderID, itemID, itemQuanity) VALUES (?, ?,?)";
-            var itemID = parseInt(req.body.itemID[i]);
-            var number = parseInt(req.body.number[i]);
-            //console.log('itemID: ' + itemID);
-            //console.log('number: ' + number);
-            if (number > 0) {
-                orderMark = 1;
-                priceToPay = priceToPay + number * parseFloat(req.body.unitPrice[i]);
-                console.log('price: ' + parseFloat(req.body.unitPrice[i]));
-                var inserts = [orderID, itemID, number];
-                console.log("inserts: " + inserts);
-                sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
-                    if (error) {
-                        console.log('Not OK');
-                        console.log(JSON.stringify(error));
-                        res.write(JSON.stringify(error));
-                        res.end();
-                    } else {
-                        console.log('OK');
-                    }
-                });
-            }
-        }
-        if(orderMark >0){
-        console.log('priceToPay: ' + priceToPay);
-        //update the totalPrice for the order
-        var sql3 = "UPDATE Orders SET totalPrice=? WHERE orderID =?";
-        var inserts3 = [priceToPay, orderID];
-                sql3 = mysql.pool.query(sql3, inserts3, function (error, results, fields) {
-                    console.log(sql3);
-                    if (error) {
-                        console.log('Not OK');
-                        console.log(JSON.stringify(error));
-                        res.write(JSON.stringify(error));
-                        res.end();
-                    } else {
-                        console.log('OK');
-                        res.redirect('/order/Confirmation');
-                    }
-                });
-
-                }
-        //user didn't order, delete the preSet for order
-        if(orderMark == 0){
-            var sql4 = "DELETE FROM Orders WHERE orderID =?";
-            var inserts4 = [orderID];
-            sql4 = mysql.pool.query(sql4, inserts4, function (error, results, fields) {
-                                console.log(sql4);
-                                if (error) {
-                                    console.log('Not OK');
-                                    console.log(JSON.stringify(error));
-                                    res.write(JSON.stringify(error));
-                                    res.end();
-                                } else {
-                                    console.log('OK');
-                                }
-                            });
-                            res.redirect('/order/Order');
-        }
 
 
     });
